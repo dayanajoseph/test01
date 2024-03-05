3d850bdfba957b19b0e7302127dd5882630f6a32
Dayana Joseph <dayanajoseph@Dayanas-MacBook-Air.local>
2024-03-04 21:57:59 -0500

    second commit

--- main.go
+++ main.go
@@ -18,5 +18,11 @@ func main() {
 2) cd test01
 // create files and run check it works
 3) git init
+4) git add 
+5) git commit -m "First comment"
+6) git status
+7) Create a github.com repository- same name as folder
+8) git remote add origin https://github.com/dayanajoseph/test01.git
+9) git remote set-url origin https://dayanajoseph:$GITHUB_TOKEN@github.com/dayanajoseph/test01.git
 
 */
