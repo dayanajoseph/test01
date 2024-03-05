b95a2a9e57da07cc6eb702abe162a0ae02e82cec
Dayana Joseph <dayanajoseph@Dayanas-MacBook-Air.local>
2024-03-04 21:47:25 -0500

    First message

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	length := 10.0 //Length of the rectangle
+	width := 5.0   //Width of the rectangle
+
+	area := length * width
+
+	fmt.Printf("The area of the rectangle is: %.2f square units.\n", area)
+
+}
+
+/*
+1) mkdir test01
+2) cd test01
+// create files and run check it works
+3) git init
+
+*/
